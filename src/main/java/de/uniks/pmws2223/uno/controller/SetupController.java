1742ae0873ad0b2b35870731f430391765d6f9c7
Henrik <henrikstarke@yahoo.de>
2023-02-12 15:38:13 +0100

    created controller

---
+++ src/main/java/de/uniks/pmws2223/uno/controller/SetupController.java
@@ -0,0 +1,27 @@
+package de.uniks.pmws2223.uno.controller;
+
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class SetupController implements Controller{
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Parent render() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
